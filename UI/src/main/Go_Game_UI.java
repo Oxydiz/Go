28480fa2e5ea79b2746d372036e6bf247cfa47d1
Maxou <martopeer@gmail.com>
2015-04-28 19:25:18 +0200

    Ajout d'un composant contenant tous les textes

--- UI/src/main/Go_Game_UI.java
+++ UI/src/main/Go_Game_UI.java
@@ -3,6 +3,7 @@ import graphics.JCanvas;
 
 import java.awt.*;
 
+import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -31,16 +32,21 @@ public class Go_Game_UI {
 		goban.setPreferredSize(new Dimension(GOBANSIZE * GRIDSIZE,GOBANSIZE * GRIDSIZE));
 		new SimpleMouseListener(goban);
 		
-		JLabel title = new JLabel("<html>Score<br> Player 1 : 0 <br> Player 2 : 0</html>");
-		title.setFont (title.getFont ().deriveFont (64.0f));
+		Box box_text = Box.createVerticalBox();
+		JLabel text_title = new JLabel("<html>Score</html>",SwingConstants.CENTER);
+		text_title.setFont (text_title.getFont ().deriveFont (64.0f));
+		box_text.add(text_title);
+		JLabel text_content = new JLabel("<html>Player 1 : 0 rock <br> Player 2 : 0 rock</html>");
+		text_content.setFont (text_content.getFont ().deriveFont (32.0f));
+		box_text.add(text_content);
 
 		//Build the main frame
-		JFrame frame = new JFrame("Jeu de go");
+		JFrame frame = new JFrame(TITLE);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		frame.setResizable(false);
 		frame.setAlwaysOnTop(true);
-		frame.add(title, BorderLayout.PAGE_END);
 		frame.add(goban, BorderLayout.LINE_START);
+		frame.add(box_text, BorderLayout.PAGE_END);		
 		frame.pack();
 		frame.setVisible(true);
 		
@@ -49,10 +55,3 @@ public class Go_Game_UI {
 		frame.setLocationRelativeTo(null);
 	} 
 }
-/*
- * List<IDrawable> selectedDrawables = canvas.findDrawables(e.getPoint());
-		if (selectedDrawables.size() == 0) return;
-		IDrawable drawable = (IDrawable) selectedDrawables.get(0);
-		canvas.removeDrawable(drawable);
- * 
- */
